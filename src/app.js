6b8b1cca51606f064b13614d02d5685bc7309807
DhannuSingh <ds2839925@gmail.com>
2026-07-09 00:48:03 +0530

    Auth middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 
-const authRouter = require('./routes/auth.routes')
+
 
 const app = express() /* creating the server */
 
@@ -10,7 +10,19 @@ const app = express() /* creating the server */
 app.use(express.json()) /* This middleware is use for express server can read the data coming from req.body */
 app.use(cookieParser())
 
+/**
+ * -Routes required
+ */
+
+const authRouter = require('./routes/auth.routes')
+const accountRouter = require('./routes/accoount.routes')
+
+
+/**
+ * - Use Routes
+ */
 app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
 
 
 
